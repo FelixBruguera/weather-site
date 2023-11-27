485c3feeca4075d9e286e589f17c2db5e0ec6953
Felix <felixbruguera0@gmail.com>
2023-11-27 14:22:42 -0400

    changed the ui and style

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ const wind = document.querySelector('#data-wind')
 const dataDiv = document.querySelector('#data')
 const userBotton = document.querySelector('#user-input')
 const userInput = document.querySelector('input')
+const currentTime = document.querySelector('#data-time')
+const feel = document.querySelector('#data-feel')
+const celsius = document.querySelector('#celsius')
+const body = document.querySelector('body')
 
 function callAPI(city) {
     const request = new XMLHttpRequest()
     request.open("GET",`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ce4998fdc9637ea68474a297f8d17084`, false);
     request.onload = function() {
-        console.log(JSON.parse(request.responseText));
         jsonData = JSON.parse(request.responseText)};
     request.send();
-    console.log('out of load')
     return jsonData;
 };
 
@@ -27,9 +29,12 @@ function clearData() {
     city.textContent = '';
     count.textContent = '';
     temp.textContent = '';
+    feel.textContent = 'Feels like: ';
     humidity.textContent = 'Humidity: ';
     wind.textContent = 'Wind: ';
-    weather.textContent = ''
+    weather.textContent = '';
+    currentTime.textContent = '';
+    celsius.textContent = '°';
 }
 
 
@@ -38,29 +43,44 @@ function toCelsius(kelvin) {
 };
 
 function showTemp(main) {
+    if (main.cod == 404) {return city.textContent = main.message}
+    let condition = main.weather.main;
     city.textContent += main.name;
     let flag = document.createElement('img')
-    let flagName = document.createElement('figcaption')
-    flag.src = `https://flagsapi.com/${main.sys.country}/flat/64.png`
-    flagName.textContent = main.sys.country;
+    flag.src = `https://flagsapi.com/${main.sys.country}/flat/32.png`
     count.appendChild(flag);
-    count.appendChild(flagName);
-    temp.textContent += `${toCelsius(main.main.temp)}°C`;
+    temp.textContent += `${toCelsius(main.main.temp)}`;
+    feel.textContent += `${toCelsius(main.main.feels_like)}°`;
     humidity.textContent += `${main.main.humidity}%`;
     wind.textContent += `${main.wind.speed} m/s`;
-    let weatherStatus = main.weather[0].main
+    let weatherStatus = main.weather[0].description
     let weatherCode = main.weather[0].icon
     let figure = document.createElement('figure')
     let weatherIcon = document.createElement('img')
     let figcaption = document.createElement('figcaption')
-    weatherIcon.src = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`
+    weatherIcon.src = `https://openweathermap.org/img/wn/${weatherCode}@4x.png`
+    weatherIcon.classList.add('weather-icon')
     figcaption.textContent = weatherStatus
+    figcaption.style.color = 'azure'
     figure.appendChild(weatherIcon);
     figure.appendChild(figcaption);
     weather.appendChild(figure);
+    let unix = main.dt;
+    let shift = main.timezone;
+    let seconds = unix + shift;
+    let miliseconds = seconds*1000
+    let date = new Date(miliseconds).toUTCString();
+    currentTime.textContent += date
 };
 
 userBotton.addEventListener('click', function() {
+    if (firstRun() == false) {clearData()};
+    let call = callAPI(userInput.value);
+    showTemp(call);
+});
+
+body.addEventListener('keydown', function(e) {
+    if (e.code != 'Enter') {return}
     if (firstRun() == false) {clearData()};
     let call = callAPI(userInput.value);
     showTemp(call);
